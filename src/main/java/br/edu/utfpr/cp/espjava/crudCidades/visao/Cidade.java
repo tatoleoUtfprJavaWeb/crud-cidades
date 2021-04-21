52151f0631fdf2494caa358826bcf1962bc4c633
Leonardo Ramos da Silva <lrsilva@celepar.pr.gov.br>
2021-04-21 12:35:47 -0300

    Deixando a página dinâmica com o Freemarker

---
+++ src/main/java/br/edu/utfpr/cp/espjava/crudCidades/visao/Cidade.java
@@ -0,0 +1,28 @@
+package br.edu.utfpr.cp.espjava.crudCidades.visao;
+
+public class Cidade {
+
+    private String nome;
+    private String estado;
+
+    public Cidade(String nome, String estado) {
+        this.nome = nome;
+        this.estado = estado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
--- src/main/java/br/edu/utfpr/cp/espjava/crudCidades/visao/CidadeController.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudCidades/visao/CidadeController.java
@@ -1,13 +1,26 @@
 package br.edu.utfpr.cp.espjava.crudCidades.visao;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
 
 @Controller
 public class CidadeController {
 
     @GetMapping("/")
-    public String listar(){
-        return "crud.html";
+    public String listar(Model model){
+
+        var cidades = Set.of(
+          new Cidade("Curitiba", "PR"),
+          new Cidade("São Paulo", "SP"),
+          new Cidade("Florianópolis", "SC")
+        );
+
+        model.addAttribute("listaCidades", cidades);
+
+        return "/crud";
     }
 }
+;
